27f7de51e14a1ab052eb330275d26f12c4aa8f04
Sebastian <powrseba@gmail.com>
2023-03-22 08:40:05 +0100

    status endpoint mapping

--- src/main/java/me/john/amiscaray/springwebsocketdemo/controllers/StatusController.java
+++ src/main/java/me/john/amiscaray/springwebsocketdemo/controllers/StatusController.java
@@ -1,12 +1,14 @@
 package me.john.amiscaray.springwebsocketdemo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/status")
 public class StatusController {
-    
-    @GetMapping({"/status"})
+
+    @GetMapping
     public String status() {
         return "Application status (OK)";
     }
