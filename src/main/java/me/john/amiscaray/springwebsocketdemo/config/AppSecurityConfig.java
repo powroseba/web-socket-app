b667b54dc0c9c91b555aee26e82eed4cd22477e6
Sebastian <powrseba@gmail.com>
2023-03-23 11:06:50 +0100

    adding necessary import

--- src/main/java/me/john/amiscaray/springwebsocketdemo/config/AppSecurityConfig.java
+++ src/main/java/me/john/amiscaray/springwebsocketdemo/config/AppSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @EnableWebSecurity
